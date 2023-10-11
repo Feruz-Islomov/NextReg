c5186d1d49647da299abd26e3bc9b5a6ab9b9cdd
Feruz <70520745+Feruz-Islomov@users.noreply.github.com>
2023-10-11 17:49:09 +0500

    checked to fire

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -3,18 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  //   apiKey: process.env.NEXT_PUBLIC_APIKEY,
-  //   authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
-  //   projectId: process.env.NEXT_PUBLIC_PROJECTID,
-  //   storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
-  //   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
-  //   appId: process.env.NEXT_PUBLIC_APPID,
-  apiKey: "AIzaSyA4-F9Zzlm147XOA4OXgT9wHGGvnXCqSoc",
-  authDomain: "thereghotel.firebaseapp.com",
-  projectId: "thereghotel",
-  storageBucket: "thereghotel.appspot.com",
-  messagingSenderId: "1041329500265",
-  appId: "1:1041329500265:web:14d5eb7264e585ecf9f2f8",
+  apiKey: process.env.NEXT_PUBLIC_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
+  appId: process.env.NEXT_PUBLIC_APPID,
 };
 
 const app = initializeApp(firebaseConfig);
