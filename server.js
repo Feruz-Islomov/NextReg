94f118f0d1fc759560974cfc6150889ab08c7cfa
Feruz <70520745+Feruz-Islomov@users.noreply.github.com>
2023-10-13 19:06:38 +0500

    config

--- server.js
+++ server.js
@@ -2,18 +2,6 @@
 const WebSocket = require("ws");
 const wss = new WebSocket.Server({ port: 3030 });
 
-// wss.on("connection", (ws) => {
-//   ws.on("message", (message) => {
-//     // Broadcast the message to all connected clients
-
-//     console.log(message);
-//     wss.clients.forEach((client) => {
-//       if (client !== ws && client.readyState === WebSocket.OPEN) {
-//         client.send(message);
-//       }
-//     });
-//   });
-// });
 wss.on("connection", (ws) => {
   ws.on("message", (message) => {
     try {
